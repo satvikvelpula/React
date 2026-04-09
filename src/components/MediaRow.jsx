1c9453330d1d6649b44e5656528532c390420e77
satvi <satvikvelpula0@gmail.com>
2026-04-10 00:37:52 +0300

    Cleanup

--- src/components/MediaRow.jsx
+++ src/components/MediaRow.jsx
@@ -1,18 +1,31 @@
-// src/components/MediaRow.jsx
-const MediaRow = (props) => {
-    const {item} = props;
-    return (
-        <tr key={item.media_id}>
-        <td>
-          <img src={item.thumbnail} alt={item.title} />
-        </td>
-        <td>{item.title}</td>
-        <td>{item.description}</td>
-        <td>{new Date(item.created_at).toLocaleString('fi-FI')}</td>
-        <td>{item.filesize}</td>
-        <td>{item.media_type}</td>
-      </tr>
-    );
-  };
+import PropTypes from 'prop-types';
+
+const MediaRow = ({ item }) => {
+  return (
+    <tr>
+      <td>
+        <img src={item.thumbnail} alt={item.title} />
+      </td>
+      <td>{item.title}</td>
+      <td>{item.description}</td>
+      <td>{new Date(item.created_at).toLocaleString('fi-FI')}</td>
+      <td>{item.filesize}</td>
+      <td>{item.media_type}</td>
+    </tr>
+  );
+};
+
+
+MediaRow.propTypes = {
+  item: PropTypes.shape({
+    media_id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string,
+    thumbnail: PropTypes.string.isRequired,
+    created_at: PropTypes.string.isRequired,
+    filesize: PropTypes.number.isRequired,
+    media_type: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
 export default MediaRow;
